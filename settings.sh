3463815f5f1033e338fa0a2e18d54098267bad34
James Sungjae Lee <odobenuseKR@gmail.com>
2022-01-10 13:47:29 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -4,7 +4,4 @@ sudo apt-get install python3-pip -y
 aws --version
 pip --version
 
-git clone https://github.com/odobenuskr/spot-health-checker.git
-cd spot-health-checker/
-mkdir logs
 pip install -r requirements.txt
